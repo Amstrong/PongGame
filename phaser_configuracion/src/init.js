e42148aa75ba42514ec35c063d7916fbba1f7813
Amstrong Monachello <amstrongmonachello@gmail.com>
2019-11-02 23:09:06 -0500

    Agregando Control por teclado

--- phaser_configuracion/src/init.js
+++ phaser_configuracion/src/init.js
@@ -7,19 +7,34 @@ const config = {
     preload: preload,
     create: create,
     update: update
+  },
+  physics: {
+    default: "arcade",
+    arcade: {
+      gravity: {
+        //y: 500
+      }
+    }
   }
 };
 
 var game = new Phaser.Game(config);
 function preload() {
-  this.pajaro = this.load.image("pajaro", "./assets/bird.png");
+  this.load.image("bird", "./assets/bird.png");
 }
 function create() {
-  this.add.image(50, 100, "pajaro");
-  this.pajaro.setScale(2);
-  this.pajaro.flipX = true;
-  this.pajaro.setOrigin(0, 0);
+  this.pajaro = this.physics.add.image(100, 50, "bird");
+  // this.input.keyboard.on("keydown_RIGHT", () => {
+  //   this.pajaro.setAcceleration(100, 0);
+  // });
+  // this.input.keyboard.on("keyup_RIGHT", () => {
+  //   this.pajaro.setAcceleration(0, 0);
+  //   this.pajaro.setVelocity(0);
+  // });
+  this.cursor = this.input.keyboard.createCursorKeys();
 }
 function update(time, delta) {
-  this.pajaro.angle++;
+  if (this.cursor.right.isDown) {
+    this.pajaro.x++;
+  }
 }
