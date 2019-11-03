0975ab74f453fd8c2179e5e931ffdeeadab20aaf
Amstrong Monachello <amstrongmonachello@gmail.com>
2019-11-03 04:00:51 -0500

    Air Hockey con movimiento

--- phaser_configuracion/src/init.js
+++ phaser_configuracion/src/init.js
@@ -1,40 +1,12 @@
+import Bootloader from "./bootloader.js";
+import Scene_play from "./scenes/scene_play.js";
 const config = {
-  widht: 320 * 2,
-  height: 180 * 2,
+  widht: 640,
+  height: 400,
   parent: "container",
-  type: Phaser.AUTO,
-  scene: {
-    preload: preload,
-    create: create,
-    update: update
-  },
   physics: {
-    default: "arcade",
-    arcade: {
-      gravity: {
-        //y: 500
-      }
-    }
-  }
+    default: "arcade"
+  },
+  scene: [Bootloader, Scene_play]
 };
-
-var game = new Phaser.Game(config);
-function preload() {
-  this.load.image("bird", "./assets/bird.png");
-}
-function create() {
-  this.pajaro = this.physics.add.image(100, 50, "bird");
-  // this.input.keyboard.on("keydown_RIGHT", () => {
-  //   this.pajaro.setAcceleration(100, 0);
-  // });
-  // this.input.keyboard.on("keyup_RIGHT", () => {
-  //   this.pajaro.setAcceleration(0, 0);
-  //   this.pajaro.setVelocity(0);
-  // });
-  this.cursor = this.input.keyboard.createCursorKeys();
-}
-function update(time, delta) {
-  if (this.cursor.right.isDown) {
-    this.pajaro.x++;
-  }
-}
+new Phaser.Game(config);
