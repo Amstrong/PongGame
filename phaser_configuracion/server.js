28eee72f408990508a7087b8386a71f7613bca3f
Amstrong <monachelloa@uninorte.edu.co>
2019-11-08 22:48:12 -0500

    Haciendo funcionar el Server.js

---
+++ phaser_configuracion/server.js
@@ -0,0 +1,24 @@
+var connections = [];
+var express = require("express");
+var app = express();
+var server = require("http").Server(app);
+var io = require("socket.io").listen(server);
+app.use("/assets", express.static(__dirname + "/assets/"));
+app.use("/src", express.static(__dirname + "/src/"));
+
+app.get("/", function(req, res) {
+  res.sendFile(__dirname + "/index.html");
+});
+server.listen(8081, function() {
+  // Listens to port 8081
+  console.log("Listening on " + server.address().port);
+});
+var socket = require("socket.io");
+var io = socket(server);
+
+io.sockets.on("connection", function(socket) {
+  connections.push(socket);
+  socket.on("start", function(data) {
+    console.log("Un usuario se ha conectado:" + connections.length);
+  });
+});
